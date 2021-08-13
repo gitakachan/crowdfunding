7521afaf4e55496bf251fd20182e518f441e45dc
Cheni Chen <celiachenic@gmail.com>
2021-08-13 12:37:30 +0800

    add product title and image

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
     <nav-bar></nav-bar>
+    <product></product>
   </div>
 </template>
 
 <script>
 import NavBar from "../src/components/navbar/NavBar.vue";
+import Product from "./components/product/Product.vue";
 export default {
   name: "App",
-  components: { NavBar },
+  components: { NavBar, Product },
 };
 </script>
 
---
+++ src/components/product/Product.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="product">
+    <div class="container pb-5">
+      <div class="row">
+        <div class="col pt-5">
+          <h5>
+            <span class="badge bg-secondary">魔法科技</span>
+          </h5>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col pb-4">
+          <h1 class="fw-bold">
+            拍出會動的照片｜<span class=" d-block d-lg-inline"
+              >LivePhotos 魔法拍立得</span
+            >
+          </h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col col-lg-7">
+          <img
+            class="img-fluid"
+            style="border-radius:40px"
+            src="../../assets/imgs/product/produt.jpg"
+            alt=""
+          />
+        </div>
+        <div class="col-5">
+          <div class="content">
+              
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss">
+.product {
+  background-color: #f8f9fa;
+}
+</style>
