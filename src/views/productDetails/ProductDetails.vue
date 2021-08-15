7fd484844f47f68758d126df60df80dbccb2195d
Chenic <celiachenic@gmail.com>
2021-08-16 00:14:32 +0800

    router view 共用style

--- src/views/productDetails/ProductDetails.vue
+++ src/views/productDetails/ProductDetails.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="product-details container overflow-hidden">
-    <div class="row">
-      <div class="details col-12 col-lg-8">
+  <div class="product-details container overflow-hidden pt-times2 pt-lg-5">
+    <div class="row gx-5">
+      <div class="details col-12 col-lg-8 pb-times2 pb-lg-5">
         <router-view></router-view>
       </div>
       <div class="plan col-12 col-lg-4"><div class="bg-primary">kk</div></div>
