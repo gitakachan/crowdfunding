03fc5daa1f67e80a215b5e4a3bddc62959d614ea
Chenic <celiachenic@gmail.com>
2021-08-17 22:33:30 +0800

    product style

--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="product">
-    <div class="container pb-5">
+    <div class="container pb-times2">
       <!-- product heders -->
       <div class="row">
         <div class="col pt-5">
@@ -20,7 +20,7 @@
       </div>
       <!-- img and content -->
       <div class="row">
-        <!-- img in the left-->
+        <!-- img on the left-->
         <div class="col col-lg-7">
           <img
             class="img-fluid"
@@ -29,16 +29,23 @@
             alt=""
           />
         </div>
-        <!-- content in the right -->
+        <!-- content on the right -->
         <div class="col-lg-5">
-          <div class="content">
-            <div class="target">
-              目標 $600,000
+          <div class="content pt-3 pt-lg-0">
+            <!-- target -->
+            <div class="target fs-14 text-dark mb-1 mb-lg-0">
+              目標 <span class="baloo">$600,000</span>
             </div>
-            <div>$280,047</div>
-            <div class="progress">
+            <div
+              class="baloo fs-times2 fw-bold"
+              style="color:#53524F; height:56px; line-height:56px"
+            >
+              $280,047
+            </div>
+            <!-- progress bar -->
+            <div class="progress mb-2 mb-lg-20">
               <div
-                class="progress-bar"
+                class="progress-bar bg-warning fs-14 fw-bold text-secondary"
                 role="progressbar"
                 style="width: 41%;"
                 aria-valuenow="25"
@@ -47,19 +54,27 @@
               >
                 41%
               </div>
+              <!-- counts -->
             </div>
-            <div class="counts d-flex">
+            <!-- conuts -->
+            <div class="counts d-flex fw-bold" style="margin-bottom:9px">
               <div class="people flex-grow-1">
-                <div>贊助人數</div>
-                <div>1374人</div>
+                <div class="text-dark fs-14">贊助人數</div>
+                <div class="fs-2 fw-bold">
+                  <!-- 數字套用baloo字體後高度比較小 所以再放大 -->
+                  <span class="baloo fs-1">1374</span> 人
+                </div>
               </div>
               <div class="countdown flex-grow-1">
-                <div>募資倒數</div>
-                <div>27天</div>
+                <div class="text-dark fs-14">募資倒數</div>
+                <div class="fs-2 fw-bold">
+                  <!-- 數字套用baloo字體後高度比較小 所以再放大 -->
+                  <span class="baloo fs-1">27</span> 天
+                </div>
               </div>
             </div>
-
-            <div class="icons text-center">
+            <!-- icons -->
+            <div class="icons text-center mb-3">
               <div class="mobile d-block d-lg-none d-flex">
                 <div
                   class="iconContainer flex-fill d-flex align-items-center"
@@ -67,7 +82,8 @@
                   :key="item.title"
                 >
                   <img :src="item.link" alt="" />
-                  <span class="mx-auto" style="font-size:12px">{{
+                  <span class="mx-auto d-md-none fs-12">{{ item.title }}</span>
+                  <span class="mx-auto d-none d-md-block fs-14">{{
                     item.title
                   }}</span>
                 </div>
@@ -85,15 +101,66 @@
                 </div>
               </div>
             </div>
-            <div>
+            <!-- message -->
+            <p
+              class="rounded bg-white border-start border-4 border-warning p-3 text-dark mb-3 mb-lg-4"
+            >
               專案募資中！<br />
-              在 2021/06/14 23:59 募資結束前，您都可以贊助我們！
-            </div>
+              在
+              <span class="baloo fw-bold fs-5" style="line-height:20px"
+                >&nbsp;2021/06/14 23:59&nbsp;</span
+              >
+              募資結束前，您都可以贊助我們！
+            </p>
 
+            <!-- buttons -->
             <div class="btns">
-              <button>贊助專案</button>
-              <button>追蹤專案</button>
-              <button>分享</button>
+              <div class="mb-2">
+                <button class="btn btn-warning rounded-pill w-100 fs-18 btn-lg">
+                  贊助專案
+                </button>
+              </div>
+              <div class="row gx-2">
+                <div class="col col-lg-7">
+                  <button
+                    class="btn btn-outline-secondary rounded-pill fs-18 w-100 btn-lg"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      class="bi bi-heart-fill me-2"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                      />
+                    </svg>
+                    追蹤專案
+                  </button>
+                </div>
+                <div class="col col-lg-5">
+                  <button
+                    class="btn btn-outline-secondary rounded-pill fs-18 w-100 btn-lg"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      class="bi bi-share-fill me-2"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"
+                      />
+                    </svg>
+                    分享
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -133,6 +200,17 @@ export default {
 };
 </script>
 <style lang="scss">
+//buttons
+$secondary: #858377;
+$danger: #ff785e;
+$warning: #ffdf65;
+
+@mixin btnHover($color, $bgColor) {
+  color: $color;
+  background-color: $bgColor;
+  border-color: $bgColor;
+}
+
 .product {
   background-color: #f8f9fa;
 
@@ -148,6 +226,17 @@ export default {
       .laptop {
         img {
           margin-right: 25px;
+        }
+      }
+    }
+    .btns {
+      .btn-warning:hover {
+        @include btnHover(#fff, $danger);
+      }
+      .btn-outline-secondary:hover {
+        @include btnHover($secondary, $warning);
+        svg {
+          color: $danger;
         }
       }
     }
