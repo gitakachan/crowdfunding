14cf225567b23a1c163606ce03caff58e374eb9a
Chenic <celiachenic@gmail.com>
2021-08-24 00:14:18 +0800

    留言組件

--- src/views/productDetails/message/Message.vue
+++ src/views/productDetails/message/Message.vue
@@ -1,11 +1,52 @@
 <template>
-  <h1>message</h1>
+  <div>
+    <messageItem :message="messages[0]">
+      <template v-slot:headIcon>
+        <div class="headIcon">
+          <img src="" alt="" />
+        </div>
+      </template>
+    </messageItem>
+  </div>
 </template>
 <script>
+import MessageItem from "./MessageItem.vue";
 export default {
-  name: "",
+  name: "Message",
   data() {
-    return {};
+    return {
+      messages: [
+        {
+          name: "廖小杰",
+          time: "2020年5月22日 11:32",
+          message:
+            "晚上起床上廁所看到照片裡的人一直動其實有點恐怖，希望可以有暫停或是定時的功能！",
+          reply: "你要嘛起床的時候開燈，要嘛給我們更多錢開發阿",
+        },
+        {
+          name: "卡阿伯",
+          time: "2020年5月22日 11:32",
+          message: "希望有更多花色可以選擇！我女兒最近喜歡綠色",
+          reply: "",
+        },
+        {
+          name: "賈師",
+          time: "2020年5月22日 11:32",
+          message:
+            "晚上起床上廁所看到照片裡的人一直動其實有點恐怖，希望可以有暫停或是定時的功能！",
+          reply: "",
+        },
+        {
+          name: "俊俊",
+          time: "2020年5月22日 11:32",
+          message: "test123 看一下我是不是真的可以留言",
+          reply: "",
+        },
+      ],
+    };
+  },
+  components: {
+    MessageItem,
   },
 };
 </script>
---
+++ src/views/productDetails/message/MessageItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="messageItem card mb-3 rounded-3">
+    <div class="question">
+      <div class=" d-flex">
+        <div class="headicon-container">
+          <slot name="headIcon"></slot>
+        </div>
+        <div>
+          <p class="name">{{ message.name }}</p>
+          <p class="time">{{ message.time }}</p>
+        </div>
+      </div>
+      <p class="message">{{ message.message }}</p>
+    </div>
+    <div class="reply" v-if="message.reply.length !== 0">
+      <p>提案者回覆</p>
+      <p>{{ message.reply }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "MessageItem",
+  data() {
+    return {};
+  },
+  props: {
+    message: {
+      type: Object,
+      //reqired: true,
+    },
+  },
+};
+</script>
+<style lang="scss"></style>
