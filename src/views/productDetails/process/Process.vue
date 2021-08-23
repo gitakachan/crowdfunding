8bf6fad0391cd40349c99d4ba384754a0936de1e
Chenic <celiachenic@gmail.com>
2021-08-23 23:46:18 +0800

    目前進度 組件完成

--- src/views/productDetails/process/Process.vue
+++ src/views/productDetails/process/Process.vue
@@ -1,12 +1,84 @@
 <template>
-  <h1>process</h1>
+  <div class="process">
+    <process-item :event="events[0]">
+      <template v-slot:bg>
+        <div
+          class="bg h-100 w-100"
+          :style="{
+            backgroundImage:
+              'url(' + require('@/assets/imgs/process/news01.jpg') + ')',
+          }"
+        ></div>
+      </template>
+    </process-item>
+    <process-item :event="events[1]">
+      <template v-slot:bg>
+        <div
+          class="bg w-100 h-100"
+          :style="{
+            backgroundImage:
+              'url(' + require('@/assets/imgs/process/news02.jpg') + ')',
+          }"
+        ></div>
+      </template>
+    </process-item>
+    <process-item :event="events[2]">
+      <template v-slot:bg>
+        <div
+          class="bg w-100 h-100"
+          :style="{
+            backgroundImage:
+              'url(' + require('@/assets/imgs/process/news03.jpg') + ')',
+          }"
+        ></div>
+      </template>
+    </process-item>
+  </div>
 </template>
 <script>
+import ProcessItem from "./ProcessItem.vue";
 export default {
-  name: "",
+  name: "Process",
   data() {
-    return {};
+    return {
+      url: "http://google.com",
+      events: [
+        {
+          title: "高雄體驗會｜免費參加送帆布袋",
+          time: "2021-6-20",
+          text:
+            "大家安安！謝謝每位贊助者的熱情支持，讓我們離夢想更進一步，我們將於下個月舉辦體驗會，歡迎各位踴躍參與！",
+          linkUrl: "#",
+        },
+        {
+          title: "高雄體驗會｜免費參加送帆布袋",
+          time: "2021-6-20",
+          text:
+            "大家安安！謝謝每位贊助者的熱情支持，讓我們離夢想更進一步，我們將於下個月舉辦體驗會，歡迎各位踴躍參與！",
+          linkUrl: "#",
+        },
+        {
+          title: "高雄體驗會｜免費參加送帆布袋",
+          time: "2021-6-20",
+          text:
+            "大家安安！謝謝每位贊助者的熱情支持，讓我們離夢想更進一步，我們將於下個月舉辦體驗會，歡迎各位踴躍參與！",
+          linkUrl: "#",
+        },
+      ],
+    };
+  },
+  components: {
+    ProcessItem,
   },
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.process {
+  .bg {
+    background-color: #ccc;
+    background-size: cover;
+    background-position: center center;
+    object-fit: cover;
+  }
+}
+</style>
---
+++ src/views/productDetails/process/ProcessItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="processItem card mb-3 rounded-3">
+    <div class="row gx-0">
+      <div class="col-4">
+        <slot name="bg"></slot>
+      </div>
+      <div class="col-8 pb-2 pb-lg-3">
+        <div class="card-body pb-0">
+          <h5 class="card-title fw-bold text-align-left fs-6">
+            {{ event.title }}
+          </h5>
+          <p class="time fs-14 mb-2 text-secondary baloo text-align-left">
+            {{ event.time }}
+          </p>
+          <p class="card-text d-none d-lg-block mb-lg-3">{{ event.text }}</p>
+          <p class="card-text ">
+            <small class="text-muted">
+              <a
+                @click.prevent
+                :href="`${event.link}`"
+                class="text-decoration-none fw-bold text-danger"
+                >MORE</a
+              >
+            </small>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ProcessItem",
+  data() {
+    return {};
+  },
+  props: {
+    event: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.processItem {
+  .img {
+    background: #ccc;
+  }
+  .card-title {
+    letter-spacing: 0.7px;
+  }
+  p.time {
+    letter-spacing: 0.49px;
+  }
+}
+</style>
