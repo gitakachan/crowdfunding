540f60b72c401570bed598183b21a5f9daa5f6bf
Chenic <celiachenic@gmail.com>
2021-10-05 11:52:39 +0800

    修正結構問題

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,25 @@
 <template>
   <div id="app">
     <nav-bar></nav-bar>
-    <product></product>
-    <product-nav-bar></product-nav-bar>
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-lg-8">
-          <product-details></product-details>
-        </div>
-        <div class="plans-lg-container col-12 col-lg-4 d-none d-lg-block">
-          <div class="sticky">
-            <div class="plans-lg">
-              <div class="sticky">
-                <company></company>
-                <plans></plans>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <support></support>
-    </div>
+    <router-view></router-view>
     <bottom-nav></bottom-nav>
   </div>
 </template>
 
 <script>
 import NavBar from "../src/components/navbar/NavBar.vue";
-import Company from "../src/components/company/Company.vue";
-import Plans from "../src/components/plans/Plans.vue";
-import Support from "../src/components/support/Support.vue";
 import BottomNav from "./components/bottomNav/BottomNav.vue";
-import Product from "./components/product/Product.vue";
-import ProductNavBar from "./components/productNavBar/ProductNavBar.vue";
 import ProductDetails from "./views/productDetails/ProductDetails.vue";
 
 import mitt from "mitt";
 const emitter = mitt();
+
 export default {
   name: "App",
   components: {
     NavBar,
-    Product,
-    ProductNavBar,
     ProductDetails,
     BottomNav,
-    Company,
-    Plans,
-    Support,
   },
   provide() {
     return {
@@ -59,16 +31,4 @@ export default {
 
 <style lang="scss">
 @import "./assets/scss/all.scss";
-body {
-  padding-bottom: 60px;
-
-  .sticky {
-    position: sticky;
-    top: 80px;
-    .plans-lg {
-      overflow: auto;
-      height: calc(100vh - 80px);
-    }
-  }
-}
 </style>
--- src/views/productDetails/ProductDetails.vue
+++ src/views/productDetails/ProductDetails.vue
@@ -1,12 +1,39 @@
 <template>
-  <div ref="productDetails" class="product-details container pt-times2 pt-lg-5">
-    <router-view></router-view>
+  <div class="product-details pb-time4">
+    <product></product>
+    <product-nav-bar></product-nav-bar>
+    <div ref="productDetails" class="container pt-times2 pt-lg-5">
+      <div class="row">
+        <div class="col-12 col-lg-8">
+          <router-view></router-view>
+          <div class="plans-sm-container d-lg-none">
+            <company></company>
+            <plans></plans>
+          </div>
+          <support></support>
+        </div>
+        <div class="plans-lg-container col-12 col-lg-4 d-none d-lg-block">
+          <div class="sticky">
+            <div class="plans-lg">
+              <company></company>
+              <plans></plans>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import Product from "@/components/product/Product.vue";
+import ProductNavBar from "@/components/productNavBar/ProductNavBar.vue";
+import Support from "@/components/support/Support.vue";
+import Company from "./company/Company.vue";
+import Plans from "./plans/Plans.vue";
 export default {
   name: "productDetails",
   inject: ["emitter"],
+  components: { Product, ProductNavBar, Support, Company, Plans },
   watch: {
     "$route.path": {
       handler() {
@@ -25,4 +52,13 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.sticky {
+  position: sticky;
+  top: 80px;
+  .plans-lg {
+    overflow: auto;
+    height: calc(100vh - 80px);
+  }
+}
+</style>
